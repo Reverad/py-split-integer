ee1b305c4af11da1166436b3ff9f7ed1f01d034e
Reverad <sektumpro@gmail.com>
2026-03-11 10:50:59 +0200

    fix

--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -10,11 +10,11 @@ def test_should_split_into_equal_parts_when_value_divisible_by_parts() -> None:
 
 
 def test_should_return_part_equals_to_value_when_split_into_one_part() -> None:
-    assert split_integer(2, 1)[0] == 2
+    assert split_integer(2, 1) == [2]
 
 
 def test_parts_should_be_sorted_when_they_are_not_equal() -> None:
-    assert split_integer(5, 3)[0] < split_integer(5, 3)[2]
+    assert split_integer(5, 3) == [1, 2, 2]
 
 
 def test_should_add_zeros_when_value_is_less_than_number_of_parts() -> None:
